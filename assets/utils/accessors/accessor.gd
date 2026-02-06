8de613d0176da20b2c27ff47a5ca806b9eceea95
Chacron1 <git@chacron.one>
2026-02-06 15:49:06 +0100

    Accessor: SOAP style value access

---
+++ assets/utils/accessors/accessor.gd
@@ -0,0 +1,10 @@
+@icon("res://assets/utils/icons/accessor.svg")
+class_name Accessor
+extends Resource
+
+signal value_changed(new_value)
+
+@export var value: Variant:
+    set(v):
+        value = v
+        value_changed.emit(value)
